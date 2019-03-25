f551093869343dc469a1352d4ee00821e42058ee
Alexander Kristoffersen <akristoffersen@berkeley.edu>
2019-03-25 16:58:25 -0700

    First commit, a Go program to practice hash tables/ other data structures.

---
+++ Stone.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Stone {
+    private Color color;
+    private int xxPos;
+    private int yyPos;
+
+    public Stone(Color color, int xxPos, int yyPos) {
+        this.color = color;
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+    }
+
+    public void draw(double scaling) {
+        StdDraw.setPenColor(color);
+        StdDraw.circle(this.xxPos * scaling, this.yyPos * scaling, 100);
+    }
+}
